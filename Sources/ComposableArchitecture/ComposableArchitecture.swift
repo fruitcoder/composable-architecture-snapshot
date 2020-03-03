6a1c209bb54dc244de6b058df624228db461dc28
Alexander Türk <alx91@me.com>
2020-03-03 13:04:14 +0100

    Sorry, was on holidays. Copy code from episode 0090

--- Sources/ComposableArchitecture/ComposableArchitecture.swift
+++ Sources/ComposableArchitecture/ComposableArchitecture.swift
@@ -94,22 +94,26 @@ public func combine<Value, Action>(
 	}
 }
 
+
+//struct CasePath<Root, Value> {
+//  let extract: (Root) -> Value?
+//  let embed: (Value) -> Root
+//}
+
+import CasePaths
+
 public func pullback<LocalValue, GlobalValue, LocalAction, GlobalAction>(
 	_ reducer: @escaping Reducer<LocalValue, LocalAction>,
 	value: WritableKeyPath<GlobalValue, LocalValue>,
-	action: WritableKeyPath<GlobalAction, LocalAction?>
+	action: CasePath<GlobalAction, LocalAction>
 ) -> Reducer<GlobalValue, GlobalAction> {
 	return { globalValue, globalAction in
-		guard let localAction = globalAction[keyPath: action] else { return [] }
+		guard let localAction = action.extract(from: globalAction) else { return [] }
 		let localEffects = reducer(&globalValue[keyPath: value], localAction)
 
 		return localEffects.map { localEffect in
-			localEffect.map { localAction -> GlobalAction in
-				var globalAction = globalAction
-				globalAction[keyPath: action] = localAction
-				return globalAction
-			}
-			.eraseToEffect()
+			localEffect.map(action.embed)
+				.eraseToEffect()
 		}
 	}
 }
